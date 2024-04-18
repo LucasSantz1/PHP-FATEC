6697e8bd2df71b5070f0ee697f07f21e0269fdec
LucasSantz1 <contato.lucassantannasil@gmail.com>
2024-04-18 00:31:46 -0300

    Delet archives main

--- exeresposta1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $valor = $_POST['valor']; //variavel recebe o valor digitado
-    if($valor > 0)
-        echo "Valor positivo";
-    elseif($valor < 0)
-        echo "Valor negativo";
-    else
-        echo "Valor igual a zero";
